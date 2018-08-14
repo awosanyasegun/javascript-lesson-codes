424af5baa64e0c357c81d1ea28cf9399add177e5
Awosanya Oluwasegun <you@example.com>
2018-08-14 16:56:47 +0300

    Rock paper scissors code updatates

--- script.js
+++ script.js
@@ -1,27 +1,8 @@
-//ask a question
-//random number for random response
-//use switch to create some random responses
-//output the response back with the question
-var question = prompt('Ask your Question ');
-var answer = 'something went wrong';
-var random = Math.floor(Math.random() * 10);
-switch(random){
-    case 0:
-	    answer = 'yes it is possible';
-		break;
-	case 1:
-	    answer = 'no, it is not possible';
-		break;
-	case 2:
-	    answer = 'probably';
-		break;
-	case 3:
-	    answer = 'Better Luck next time';
-		break;
-	case 4:
-	    answer = 'Good luck with your life'
-		break;
-	default:
-	answer = 'you are completely of the tangent';
-};
-output.innerHTML = 'your question was ' + '" ' + question + ' "'+ ' But my answer is ' + answer; 
+var player = prompt('Do you choose Rock paper or scissors?');
+var ps = player.charAt(0).toUpperCase();
+var win = false;
+var computer =Math.random();
+if(computer <0.34){computer = 'Rock';}
+else if (computer <0.67){computer ='paper';}
+else{computer = 'Scissors';}
+var cs = computer.charAt(0).toUpperCase();
